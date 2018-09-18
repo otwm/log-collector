0cfbb08f3a37e10434e9c1f405b83e751c60e5a2
dong o kim <otwmoo@gmail.com>
2018-09-18 16:02:22 +0900

    * 테이블 생성 코드 노출

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import ErrorReporter from './ErrorReporter';
+import { initErrorReportTable, initUserTrackingTable } from './utils/initTable';
 
 export default ErrorReporter;
+
+export {
+    initErrorReportTable,
+    initUserTrackingTable,
+};
